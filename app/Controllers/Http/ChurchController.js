25679790b58f4947a0a827b93ee22768fc5cec8c
Lucas Alves <lucas.alves@udf.org.br>
2020-02-28 15:19:52 -0300

    feat: search organizations by city and uf

--- app/Controllers/Http/ChurchController.js
+++ app/Controllers/Http/ChurchController.js
@@ -1,15 +1,6 @@
 'use strict'
 
-const axios = require('axios')
-
-const api = axios.default.create({
-  baseURL: 'https://5260046.restlets.api.netsuite.com/app/site/hosting',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization:
-      'NLAuth nlauth_account=5260046, nlauth_email=dev@udf.org.br, nlauth_signature=Shalom1234,nlauth_role=1077'
-  }
-})
+const Organization = use('App/Models/Organization')
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
@@ -20,38 +11,6 @@ const api = axios.default.create({
  */
 class ChurchController {
   /**
-   * Show a list of all churches.
-   * GET churches
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {}
-
-  /**
-   * Render a form to be used for creating a new church.
-   * GET churches/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {}
-
-  /**
-   * Create/save a new church.
-   * POST churches
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {}
-
-  /**
    * Display a single church.
    * GET churches/:id
    *
@@ -60,64 +19,33 @@ class ChurchController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params }) {
+  async show ({ response, params }) {
+    const { uf, city, name } = params
+
     try {
-      const response = await api.get(
-        `/restlet.nl?script=124&deploy=1&uf=${params.uf}&city=${
-          params.city
-        }&name=${params.name}`
-      )
-
-      if (response.data.length === 0) {
-        return {
-          error: {
-            title: 'Falha!',
-            message: 'Nenhuma igreja encontrada'
+      const church = await Organization.query()
+        .where(function () {
+          if (uf) {
+            this.where('uf', uf)
           }
-        }
-      }
 
-      return response.data
+          if (city) {
+            this.where('city', city)
+          }
+
+          if (name) {
+            this.where('name', name)
+          }
+        })
+
+      return church
     } catch (err) {
       return response.status(err.status).send({
-        error: {
-          title: 'Falha!',
-          message: 'Erro encontrado, tente novamente'
-        }
+        title: 'Falha!',
+        message: 'Houve um erro ao buscar a igreja.'
       })
     }
   }
-
-  /**
-   * Render a form to update an existing church.
-   * GET churches/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {}
-
-  /**
-   * Update church details.
-   * PUT or PATCH churches/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {}
-
-  /**
-   * Delete a church with id.
-   * DELETE churches/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {}
 }
 
 module.exports = ChurchController
