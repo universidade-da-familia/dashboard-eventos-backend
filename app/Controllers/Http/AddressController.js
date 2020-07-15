248867509bf8649f15d77aa60bd9a358437be0d2
Lucas Alves <lucas.alves@udf.org.br>
2020-07-15 15:12:26 -0300

    Fix: change netsuite user password

--- app/Controllers/Http/AddressController.js
+++ app/Controllers/Http/AddressController.js
@@ -20,7 +20,7 @@ const api = axios.default.create({
   headers: {
     'Content-Type': 'application/json',
     Authorization:
-      'NLAuth nlauth_account=5260046, nlauth_email=dev@udf.org.br, nlauth_signature=Shalom1234,nlauth_role=1077'
+      'NLAuth nlauth_account=5260046, nlauth_email=dev@udf.org.br, nlauth_signature=0rZFiwRE#@!,nlauth_role=1077'
   }
 })
 
