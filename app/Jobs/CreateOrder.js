a971898810eed882adc024dc4fb2638242e6d55a
Your Name <you@example.com>
2020-10-23 15:17:58 -0300

    fix: teste ajuste script netsuite

--- app/Jobs/CreateOrder.js
+++ app/Jobs/CreateOrder.js
@@ -1,56 +1,59 @@
-'use strict'
+"use strict";
 
-const Order = use('App/Models/Order')
+const Order = use("App/Models/Order");
 
-const axios = require('axios')
+const axios = require("axios");
+
+const { OAuth } = use("App/Helpers/netsuite_api");
 
 const api = axios.default.create({
-  baseURL: 'https://5260046.restlets.api.netsuite.com/app/site/hosting',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization:
-      'NLAuth nlauth_account=5260046, nlauth_email=dev@udf.org.br, nlauth_signature=0rZFiwRE#@!,nlauth_role=1077'
-  }
-})
+  baseURL: "https://5260046.restlets.api.netsuite.com/app/site/hosting",
+});
 
 class CreateOrder {
   // If this getter isn't provided, it will default to 1.
   // Increase this number to increase processing concurrency.
-  static get concurrency () {
-    return 1
+  static get concurrency() {
+    return 1;
   }
 
   // This is required. This is a unique key used to identify this job.
-  static get key () {
-    return 'CreateOrder-job'
+  static get key() {
+    return "CreateOrder-job";
   }
 
   // This is where the work is done.
-  async handle ({ orderNetsuite, order_id }) {
-    console.log('CreateOrder-job started')
+  async handle({ orderNetsuite, order_id }) {
+    console.log("CreateOrder-job started");
 
     const response = await api.post(
-      '/restlet.nl?script=185&deploy=1',
-      orderNetsuite
-    )
+      "/restlet.nl?script=185&deploy=1",
+      orderNetsuite,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: OAuth,
+        },
+      }
+    );
 
-    console.log(response.data)
+    console.log(response.data);
 
     if (response.data.id) {
-      console.log('Chamada ao netsuite finalizada com sucesso (CreateOrder).')
-      const order = await Order.findOrFail(order_id)
+      console.log("Chamada ao netsuite finalizada com sucesso (CreateOrder).");
+      const order = await Order.findOrFail(order_id);
 
-      order.netsuite_id = response.data.id || order.netsuite_id
+      order.netsuite_id = response.data.id || order.netsuite_id;
 
-      await order.save()
+      await order.save();
     } else {
-      console.log('Chamada ao netsuite finalizada com falha (CreateOrder).')
+      console.log("Chamada ao netsuite finalizada com falha (CreateOrder).");
       throw new Error({
-        title: 'Falha!',
-        message: 'Houve um erro ao gerar o pedido no Netsuite.'
-      })
+        title: "Falha!",
+        message: "Houve um erro ao gerar o pedido no Netsuite.",
+      });
     }
   }
 }
 
-module.exports = CreateOrder
+module.exports = CreateOrder;
