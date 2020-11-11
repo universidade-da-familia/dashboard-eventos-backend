9b531b6c4a19b5f8aace39ab3617878338752d79
Your Name <you@example.com>
2020-11-11 15:03:49 -0300

    fix: adjust order transaction

--- app/Jobs/ApproveOrder.js
+++ app/Jobs/ApproveOrder.js
@@ -1,49 +1,50 @@
-'use strict'
+"use strict";
 
-const axios = require('axios')
+const axios = require("axios");
 
 const api = axios.default.create({
-  baseURL: 'https://5260046.restlets.api.netsuite.com/app/site/hosting',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization:
-      'NLAuth nlauth_account=5260046, nlauth_email=dev@udf.org.br, nlauth_signature=0rZFiwRE#@!,nlauth_role=1077'
-  }
-})
+  baseURL: "https://5260046.restlets.api.netsuite.com/app/site/hosting",
+});
 
 class ApproveOrder {
   // If this getter isn't provided, it will default to 1.
   // Increase this number to increase processing concurrency.
-  static get concurrency () {
-    return 1
+  static get concurrency() {
+    return 1;
   }
 
   // This is required. This is a unique key used to identify this job.
-  static get key () {
-    return 'ApproveOrder-job'
+  static get key() {
+    return "ApproveOrder-job";
   }
 
   // This is where the work is done.
-  async handle ({ orderNetsuite }) {
-    console.log('ApproveOrder-job started')
+  async handle({ orderNetsuite, OAuth }) {
+    console.log("ApproveOrder-job started");
 
     const response = await api.put(
-      '/restlet.nl?script=190&deploy=1',
-      orderNetsuite
-    )
+      "/restlet.nl?script=190&deploy=1",
+      orderNetsuite,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: OAuth,
+        },
+      }
+    );
 
-    console.log(response.data)
+    console.log(response.data);
 
     if (response.data.id) {
-      console.log('Chamada ao netsuite finalizada com sucesso (ApproveOrder).')
+      console.log("Chamada ao netsuite finalizada com sucesso (ApproveOrder).");
     } else {
-      console.log('Chamada ao netsuite finalizada com falha (ApproveOrder).')
+      console.log("Chamada ao netsuite finalizada com falha (ApproveOrder).");
       throw new Error({
-        title: 'Falha!',
-        message: 'Houve um erro ao atualizar o pedido no Netsuite.'
-      })
+        title: "Falha!",
+        message: "Houve um erro ao atualizar o pedido no Netsuite.",
+      });
     }
   }
 }
 
-module.exports = ApproveOrder
+module.exports = ApproveOrder;
